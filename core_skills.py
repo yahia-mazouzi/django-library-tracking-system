8e2b91e5f90149e104c2135b2d53235d3b8703f9
Yahia Mazouzi <yahia.mazouzi@pinely.eu>
2025-04-03 15:36:58 +0100

    core skills task

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,11 @@
 import random
-# rand_list =
+rand_list = [random.randint(1 , 20 ) for _ in range(10)]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = [ item for item in rand_list if item < 10]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10_using_filter = list(filter(lambda x : x < 10 , rand_list))
+
+
+print(rand_list)
+print(list_comprehension_below_10)
+print(list_comprehension_below_10_using_filter)
